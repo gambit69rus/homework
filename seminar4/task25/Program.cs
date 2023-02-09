7df1a07ff0b23ec93a8936d22965ff59a2942f88
antonsosnin69 <oliverquine@yandex.ru>
2023-02-09 17:57:42 +0300

    add task25

---
+++ seminar4/task25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите числоA: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите числоB: ");
+int numberB = int.Parse(Console.ReadLine()!);
+ToDegree(numberA, numberB);
+
+// Функция возведения в степень
+void ToDegree(int numberA, int numberB)
+{
+    int result = 1;
+    for (int count = 1; count <= numberB; count++)
+    {
+        result = result * numberA;
+    }
+    Console.WriteLine(result);
+}
