17d59f144d8272bb8f3234a570dee9279565361e
antonsosnin69 <oliverquine@yandex.ru>
2023-02-09 19:05:06 +0300

    add task29

---
+++ seminar4/task29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт размер массива и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+Console.Write("введите размер массива: ");
+int lenArray = int.Parse(Console.ReadLine()!);
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(0, 100);
+    Console.Write(randomArray[i] + " ");
+}
+
