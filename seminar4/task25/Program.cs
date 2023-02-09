c5363e3304971a406e36fdca1a5f20068115b696
antonsosnin69 <oliverquine@yandex.ru>
2023-02-09 18:36:07 +0300

    add task27

--- seminar4/task25/Program.cs
+++ seminar4/task25/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.Write("Введите числоA: ");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите числоA: ");
 int numberA = int.Parse(Console.ReadLine()!);
 Console.Write("Введите числоB: ");
 int numberB = int.Parse(Console.ReadLine()!);
---
+++ seminar4/task27/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+int NumberLen(int number)
+{
+    int index = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        index++;
+    }
+    return index;
+}
+void SumNumbers(int number, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    Console.WriteLine(sum);
+}
