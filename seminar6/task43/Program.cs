569b9b6775b0d7796c7b051096132c69eaf40840
antonsosnin69 <oliverquine@yandex.ru>
2023-02-15 19:15:56 +0300

    add seminar6

--- seminar6/task43/Program.cs
+++ seminar6/task43/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("введите значение b1");
-double b1 = Convert.ToInt32(Console.ReadLine());
+double b1 = double.Parse(Console.ReadLine());
 Console.WriteLine("введите число k1");
-double k1 = Convert.ToInt32(Console.ReadLine());
+double k1 = double.Parse(Console.ReadLine());
 Console.WriteLine("введите значение b2");
-double b2 = Convert.ToInt32(Console.ReadLine());
+double b2 = double.Parse(Console.ReadLine());
 Console.WriteLine("введите число k2");
-double k2 = Convert.ToInt32(Console.ReadLine());
+double k2 = double.Parse(Console.ReadLine());
 
 double x = (-b2 + b1) / (-k1 + k2);
 double y = k2 * x + b2;
